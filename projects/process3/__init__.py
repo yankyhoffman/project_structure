68555ab4e52e1a26fed30e88900bc6c3a6002853
Yanky Hoffman <developer.yankyhoffman@gmail.com>
2019-08-06 17:21:04 -0400

    Add logging to folder-type project

--- projects/process3/__init__.py
+++ projects/process3/__init__.py
@@ -1,1 +1,6 @@
+from utils.logger import get_project_logger
+
+logger = get_project_logger(__name__)
+logger.debug(f"Logger initiated for {__name__}")
+
 from . import process3
--- projects/process3/process3.py
+++ projects/process3/process3.py
@@ -7,10 +7,12 @@ from utils.source import get
 from utils.remote_dest import put
 
 from projects import register_task
+from . import logger
 from ._helper import replace_with
 
 @register_task(kwargs={'replace_with': replace_with})
 def process3(replace_with):
     raw = get()
     for record in raw:
+        logger.debug(f"Processing record '{record}'")
         put(record.replace('raw', replace_with))
